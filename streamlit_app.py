4b75ab442c59c9f1b98b9e07e8d7ab4a4ae23b2d
Damian Collier <damian.collier@hotmail.com>
2022-11-01 12:27:08 +0000

    Update streamlit_app.py
    Add watermelon parts as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes data to the screen
+
+# work out each part
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display table of parts
+streamlit.dataframe(fruityvice_normalized)
